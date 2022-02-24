95c0895705363edb637347b73e8c63685457ed78
TerminatorxxD <tuanlinhdoan2@gmail.com>
2022-02-24 18:35:00 +0700

    Create new Student

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class StudentController extends AbstractController
 {
     /**
-     * @Route("/student", name="student")
+     * @Route("/student", name="student_list")
      */
     public function showAllStudent()
     {
@@ -24,13 +27,32 @@ class StudentController extends AbstractController
      * @Route("/student/{id}", name="student_show", methods={"GET"})
      */
 
-    public function viewDetail($id){
-//        $em = $this->getDoctrine()->getManager();
-        $em = $this->getDoctrine()->getRepository(Student::class);
-        $student= $em->find($id);
-        return $this->render('student/show.html.twig', ['student'=> $student]);
+//    public function viewDetail($id){
+//        $em = $this->getDoctrine()->getRepository(Student::class);
+//        $student= $em->find($id);
+//        return $this->render('student/show.html.twig', array('student' => $student));
+//    }
+
+    /**
+     * @Route("/student/create", name="student_create", methods={"GET","POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $student = new Student();
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($student);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('student_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('student/create.html.twig', [
+            'category' => $student,
+            'form' => $form,
+        ]);
     }
-
-
 
 }
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Major;
 use App\Entity\Student;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +17,12 @@ class StudentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('FullName')
-            ->add('DoB')
-            ->add('MajorID')
-        ;
+        ->add('FullName', TextType::class )
+        ->add('DoB', DateType::class,['widget' => 'single_text'])
+        ->add('MajorID', EntityType::class, [
+            'class' => Major::class,
+            'choice_label' => 'id'
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
